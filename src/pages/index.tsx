90c97452349f5a5d1f27a924a767a6723fcc758f
stonega <xijieyin@gmail.com>
2023-04-22 22:39:00 +0800

    fix: update style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,47 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/episodes/1">
+    <>
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/episodes/1"
+            >
               开始吧
-          </Link>
+            </Link>
+          </div>
         </div>
+      </header>
+      <div className={styles.recommend}>
+        你可以使用
+        <a href="https://recos.vercel.app" target="_blank">
+          Recos
+        </a>
+        将你喜欢的播客转为文字
       </div>
-      <div className="recommend">你可以使用<a href="https://recos.vercel.app" target="_blank">Recos</a>将你喜欢的播客转为文字。</div>
-    </header>
+    </>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
     </Layout>
   );
